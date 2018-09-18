6184f20611024c579f5d79d45810219944245092
zhuqb <zhuqb@huntkey.net>
2018-09-18 18:00:13 +0800

    集成redis和httpSession来共享session信息

--- amos/baseinfo/src/main/java/com/xrea/amos/baseinfo/BaseinfoApplication.java
+++ amos/baseinfo/src/main/java/com/xrea/amos/baseinfo/BaseinfoApplication.java
@@ -1,19 +1,34 @@
 package com.xrea.amos.baseinfo;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class BaseinfoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseinfoApplication.class, args);
     }
-
-    OkHttpClient okHttpClient = new OkHttpClient();
-    final Request request = new Request.Builder().url("").get().build();
+    @Value("${foo.test}")
+    String foo;
+    @RequestMapping("/sayhi")
+    public String sayHi(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("userinfo");
+        if (StringUtils.isEmpty(username)){
+            username = ""+System.currentTimeMillis();
+            request.setAttribute("userinfo",username);
+        }
+        return "hello " + username;
+    }
 }
